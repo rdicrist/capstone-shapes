05291629ea4a9b0c7a08b8f6bff900d252bf43c3
bsumma <bsumma@sci.utah.edu>
2017-08-31 14:29:34 -0500

    Added more comments and some minor updates

--- source/color.cpp
+++ source/color.cpp
@@ -9,28 +9,28 @@
 
 using namespace Angel;
 
-GLuint program;
-GLuint tri_vao;
-GLuint sq_vao;
 
-enum{_RENDER_TRIANGLE, _RENDER_SQUARE, _NUM_RENDER_OBJ};
-int current_draw = _RENDER_TRIANGLE;
+//Globals there is a way to avoid this, but for now let's do the bad thing.
+enum{_TRIANGLE, _SQUARE, _NUM_OBJ};
+int current_object;
 
-
+//GLFW error callback
 static void error_callback(int error, const char* description)
 {
   fprintf(stderr, "Error: %s\n", description);
 }
 
+//GLFW keypress callback
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GLFW_TRUE);
   if (key == GLFW_KEY_UP && action == GLFW_PRESS)
-    current_draw = (current_draw+1)%_NUM_RENDER_OBJ;
+    current_object = (current_object+1)%_NUM_OBJ;
 }
 
-void init(){
+//Initialize everything
+void init(GLuint &glShaderProgram, GLuint &triVao, GLuint &sqVao){
   
   GLuint vertex_shader, fragment_shader;
   GLint vpos_location, vcolor_location;
@@ -64,40 +64,42 @@ void init(){
                                 vec2(0.57, -0.18),
                                 vec2(0.90, 0.12)};
   
-
+  //String with absolute paths to our shaders, shader_path set by CMAKE hack
   std::string vshader = shader_path + "vshader.glsl";
   std::string fshader = shader_path + "fshader.glsl";
   
+  //Read in shader code to a character array
   GLchar* vertex_shader_source = readShaderSource(vshader.c_str());
   GLchar* fragment_shader_source = readShaderSource(fshader.c_str());
   
+  //Create and compile vertex shader
   vertex_shader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertex_shader, 1, (const GLchar**) &vertex_shader_source, NULL);
   glCompileShader(vertex_shader);
   check_shader_compilation(vshader, vertex_shader);
   
+  //Create and compile fragment shader
   fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragment_shader, 1, (const GLchar**) &fragment_shader_source, NULL);
   glCompileShader(fragment_shader);
   check_shader_compilation(fshader, fragment_shader);
   
-  program = glCreateProgram();
-  glAttachShader(program, vertex_shader);
-  glAttachShader(program, fragment_shader);
+  //Create shader program from the 2 shaders
+  glShaderProgram = glCreateProgram();
+  glAttachShader(glShaderProgram, vertex_shader);
+  glAttachShader(glShaderProgram, fragment_shader);
+  glLinkProgram(glShaderProgram);
+  check_program_link(glShaderProgram);
   
-  glLinkProgram(program);
-  check_program_link(program);
-  
-  glBindFragDataLocation(program, 0, "fragColor");
-  
-  vpos_location   = glGetAttribLocation(program, "vPos");
-  vcolor_location = glGetAttribLocation( program, "vColor" );
-  
+  //bind or find the numerical location for shader variables
+  glBindFragDataLocation(glShaderProgram, 0, "fragColor");
+  vpos_location   = glGetAttribLocation(glShaderProgram, "vPos");
+  vcolor_location = glGetAttribLocation(glShaderProgram, "vColor" );
   
   // Create a vertex array object
-  glGenVertexArrays( 1, &tri_vao );
+  glGenVertexArrays( 1, &triVao );
   //Set GL state to use vertex array object
-  glBindVertexArray( tri_vao );
+  glBindVertexArray( triVao );
   
   //Generate buffer to hold our vertex data
   glGenBuffers( 1, &tri_buffer );
@@ -121,9 +123,9 @@ void init(){
   
   
   // Create a vertex array object
-  glGenVertexArrays( 1, &sq_vao );
+  glGenVertexArrays( 1, &sqVao );
   //Set GL state to use vertex array object
-  glBindVertexArray( sq_vao );
+  glBindVertexArray( sqVao );
   
   //Generate buffer to hold our vertex data
   glGenBuffers( 1, &sq_buffer );
@@ -146,6 +148,7 @@ void init(){
   glBindVertexArray(0);
 }
 
+//Animation callback.  For now this does nothing
 void animate(){
   //Do something every second
   if(glfwGetTime() > 1.0){
@@ -155,70 +158,93 @@ void animate(){
   }
 }
 
+//Main
 int main(void)
 {
   GLFWwindow* window;
+  GLuint glShaderProgram;
+  GLuint triVao;
+  GLuint sqVao;
   
+  //Set the error callback defined above
   glfwSetErrorCallback(error_callback);
   
+  //Init glfw
   if (!glfwInit())
     exit(EXIT_FAILURE);
   
+  //Enforce OpenGL 3.2
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   
+  //Multisampling
   glfwWindowHint(GLFW_SAMPLES, 4);
 
-  
-  window = glfwCreateWindow(512, 512, "Rainbow triangle", NULL, NULL);
+  //Create window
+  window = glfwCreateWindow(512, 512, "Rainbow triangle and friends", NULL, NULL);
   if (!window){
     glfwTerminate();
     exit(EXIT_FAILURE);
   }
   
+  //Set key callback
   glfwSetKeyCallback(window, key_callback);
   
+  //GLFW setup calls
   glfwMakeContextCurrent(window);
   gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
   glfwSwapInterval(1);
   
-  init();
+  //Init the geomerty and shaders
+  init(glShaderProgram, triVao, sqVao);
   
+  //set the background/clear color
   glClearColor(1.0, 1.0, 1.0, 1.0);
   
+  //main loop, loop forever until user closes window
   while (!glfwWindowShouldClose(window)){
     
+    //Grab window size in pixels
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
+   
+    //Set viewport
     glViewport(0, 0, width, height);
     
+    //Call animation update
     animate();
     
+    //Clear color buffer bit
     glClear(GL_COLOR_BUFFER_BIT);
     
-    glUseProgram(program);
+    //Enable our shader program
+    glUseProgram(glShaderProgram);
     
-    switch(current_draw){
-      case _RENDER_TRIANGLE:
-        glBindVertexArray( tri_vao );
+    //Draw
+    switch(current_object){
+      case _TRIANGLE:
+        glBindVertexArray( triVao );
         glDrawArrays(GL_TRIANGLES, 0, 3);
         break;
-      case _RENDER_SQUARE:
-        glBindVertexArray( sq_vao );
+      case _SQUARE:
+        glBindVertexArray( sqVao );
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
         break;
      }
+    glBindVertexArray(0);
     
-    
+    //Swap our double buffers
     glfwSwapBuffers(window);
+    
+    //GLFW event handler
     glfwPollEvents();
   
   }
   
+  //Clean up
   glfwDestroyWindow(window);
-  
   glfwTerminate();
   exit(EXIT_SUCCESS);
 }
